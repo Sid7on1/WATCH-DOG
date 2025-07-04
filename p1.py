b99a5e49371f71d12b8b98bdf87a66cfa94a2e82
Siddharth Vishwanath <138299704+Sid7on1@users.noreply.github.com>
2025-07-04 21:07:47 +0530

    Update p1.py

--- p1.py
+++ p1.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 # === ENV SETUP ===
 load_dotenv()
-GITHUB_USERNAME = os.getenv("GITHUB_USERNAME")
-GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+GITHUB_USERNAME = os.getenv("USERNAME_GITHUB")
+GITHUB_TOKEN = os.getenv("TOKEN_GITHUB")
 WORKSPACE_DIR = "workspace"
 RELEVANT_JSON_DIR = "relevant_json"
 PUSH_LOG_FILE = "pushed_repos.txt"
